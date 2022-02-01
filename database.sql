73273c4ad964fc0d3fa63615638e7e5e8b25cda3
Jovanny Rch <jovannyrch@gmail.com>
2022-02-01 13:35:54 -0600

    Update columns type

--- database.sql
+++ database.sql
@@ -1,22 +1,21 @@
+drop database if exists contratos_db;
 
+create database contratos_db;
+
+use contratos_db;
 
 create table contratos(
     id_contrato int primary key not null auto_increment,
     no_expediente varchar(20) unique,
     cliente varchar(50),
     responsable_ejecucion varchar(50),
-    fecha_inicio varchar(50)
-);
-
-create table contratos(
-    id_contrato int primary key not null auto_increment,
-    titulo varchar(50) unique,
-    descripcion varchar(50)
+    fecha_inicio varchar(50),
+    path varchar(200)
 );
 
 create table anexos (
     id_anexos int primary key not null auto_increment,
-    path blob (100),
+    path varchar(200),
     nombre varchar (50),
     id_contrato int,
     foreign key(id_contrato)references contratos(id_contrato) on delete cascade
