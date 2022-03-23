be32fd90f0fea600abd220c2bc924c55fd337721
Jovanny Rch <jovannyrch@gmail.com>
2022-03-22 23:31:11 -0600

    Add logo

--- headers/header_admin.php
+++ headers/header_admin.php
@@ -16,8 +16,10 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <a class="navbar-brand" href="contratos.php">Sistema de contratos</a>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <a class="navbar-brand" href="contratos.php">
+            <img src="assets/logo.jpeg" height="40px" />
+        </a>
         <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
